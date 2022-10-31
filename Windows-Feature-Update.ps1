549212cf6bb17ef084ae7318a1c1064862644595
patbman <patbman5@gmail.com>
2022-10-31 07:59:39 -0400

    edits

--- Windows-Feature-Update.ps1
+++ Windows-Feature-Update.ps1
@@ -3,8 +3,8 @@
 $dir = 'C:\_Windows_FU\packages'
 mkdir $dir
 $webClient = New-Object System.Net.WebClient
-$url = 'https://go.microsoft.com/fwlink/?LinkID=799445'
-$file = "$($dir)\Win10Upgrade.exe"
+$url = 'https://github.com/patbman/Windows-Feature-Update/blob/main/Windows10Upgrade21H2.exe?raw=true'
+$file = "$($dir)\Win1021h2Upgrade.exe"
 $webClient.DownloadFile($url,$file)
 Start-Process -FilePath $file -ArgumentList '/quietinstall /skipeula /auto upgrade /norestart /wait'
 
---
+++ windows-feature-update-21H2.ps1
@@ -0,0 +1,26 @@
+#Windows Feature Update Script
+#Add-Type -AssemblyName PresentationFramework
+$dir = 'C:\_Windows_FU\packages'
+$file = "$($dir)\Win1021h2Upgrade.exe"
+if (!(Test-Path "$dir")){
+#$dir = 'C:\_Windows_FU\packages'
+    mkdir $dir
+    $webClient = New-Object System.Net.WebClient
+    $url = 'https://github.com/patbman/Windows-Feature-Update/blob/main/Windows10Upgrade21H2.exe?raw=true'
+    
+    $webClient.DownloadFile($url,$file)
+
+    Start-Process -FilePath $file -ArgumentList '/quietinstall /skipeula /auto upgrade /norestart /wait'
+    #start-sleep -Seconds 5
+    #Wait-Process -name "Windows10UpgraderApp"
+
+    
+    }
+else{
+    Start-Process -FilePath $file -ArgumentList '/quietinstall /skipeula /auto upgrade /norestart /wait'
+    #start-sleep -Seconds 5
+    #Wait-Process -name "Windows10UpgraderApp"
+
+}
+#$updateProcess = (Get-Process "Windows10UpgraderApp").Id
+#[System.Windows.MessageBox]::Show('Your System has been updated to the lasted version of windows please reboot your system')
